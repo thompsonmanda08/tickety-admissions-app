13410259750557d31e0b0080c77738205032693d
Thompson Manda <thompsonmanda08@outlook.com>
2024-06-24 09:29:43 +0200

    fix: add refresh function on events page

--- lib/ui/pages/events/page.dart
+++ lib/ui/pages/events/page.dart
@@ -26,6 +26,7 @@ class EventsPage extends GetView<EventsController> {
       child: GestureDetector(
         onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
         child: Scaffold(
+          backgroundColor: kPrimaryColor,
           appBar: CustomAppBar(
             leading: Avatar(
               firstName: controller.session.firstName,
@@ -61,61 +62,73 @@ class EventsPage extends GetView<EventsController> {
               ),
             ),
           ),
-          body: SingleChildScrollView(
-            child: Container(
-              constraints: BoxConstraints(minHeight: screenSize.height * 0.5),
-              child: Column(
-                children: [
-                  const MainHeader(
-                    title: "Manage Event",
-                    subtitle:
-                        "Choose an event to manage, you can change events at any time",
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(24.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Select an event',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
-                            fontStyle: FontStyle.normal,
-                            color: kPrimaryColor,
-                          ),
-                        ),
-                        const SizedBox(height: 10.0),
-                        SearchableDropdownMenu(
-                          hintText: "Select event",
-                          isDynamicSearch: true,
-                          displayKey: "eventName",
-                          searchableItems:
-                              controller.events.map((event) => event).toList(),
-                          handleSelectItem: (value) {
-                            final selectedValue = value.item;
-                            controller.selectedEventName.value =
-                                selectedValue['eventName'];
-                            controller.event.value = selectedValue;
-                          },
-                        ),
-                        const SizedBox(height: 20.0),
-                        Obx(
-                          () {
-                            bool isValid = fieldsFilledAndValid();
-                            return CustomButton(
-                              isDisabled: isValid,
-                              text: 'Continue',
-                              isLoading: controller.isLoading.value,
-                              handleOnClick:
-                                  controller.handleChangeSessionEvent,
-                            );
-                          },
-                        ),
-                      ],
+          body: RefreshIndicator(
+            backgroundColor: kPrimaryColor.withOpacity(0),
+            color: Colors.white,
+            displacement: 20,
+            edgeOffset: 10,
+            onRefresh: () async {
+              await controller.getEvents();
+            },
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Container(
+                color: Colors.white,
+                constraints: BoxConstraints(minHeight: screenSize.height * 0.9),
+                child: Column(
+                  children: [
+                    const MainHeader(
+                      title: "Manage Event",
+                      subtitle:
+                          "Choose an event to manage, you can change events at any time",
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Select an event',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              fontStyle: FontStyle.normal,
+                              color: kPrimaryColor,
+                            ),
+                          ),
+                          const SizedBox(height: 10.0),
+                          SearchableDropdownMenu(
+                            hintText: "Select event",
+                            isDynamicSearch: true,
+                            displayKey: "eventName",
+                            searchableItems: controller.events
+                                .map((event) => event)
+                                .toList(),
+                            handleSelectItem: (value) {
+                              final selectedValue = value.item;
+                              controller.selectedEventName.value =
+                                  selectedValue['eventName'];
+                              controller.event.value = selectedValue;
+                            },
+                          ),
+                          const SizedBox(height: 20.0),
+                          Obx(
+                            () {
+                              bool isValid = fieldsFilledAndValid();
+                              return CustomButton(
+                                isDisabled: isValid,
+                                text: 'Continue',
+                                isLoading: controller.isLoading.value,
+                                handleOnClick:
+                                    controller.handleChangeSessionEvent,
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
